d50ebc98fdbb56b511b541206ea8ef2ef83c2039
vanshbulchandani <139048048+vanshbulchandani@users.noreply.github.com>
2025-06-08 18:27:04 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use("/test", (req, res) => {
-  res.send("hello from test is the best");
+  res.send("hello from test is the best route for testing");
 });
 
 app.use((req, res) => {
